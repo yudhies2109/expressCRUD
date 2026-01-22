3559a20cf408684f12afedce24ad44feaf34ebce
yudhies2109 <ma.yudhistira@hotmail.com>
2026-01-22 15:44:19 +0700

    added new router added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/add', function(req, res, next) {
+    res.render('form/view');
+});
+
 
 module.exports = router;
