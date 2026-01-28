035be55f185a13071132ebaabe2069de9a17e3cc
yudhies2109 <ma.yudhistira@hotmail.com>
2026-01-28 09:34:40 +0700

    aded route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    const filePath = path.join(__dirname, '../data/data.json');
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Gagal membaca data');
+        }
+
+        const users = JSON.parse(data);
+
+        res.render('index', {
+            title: 'Data users',
+            users
+        })
+    })
+
 });
 
 router.get('/add', function(req, res, next) {
