a26cf2df8a124f6012da7e16a34cc932a2cff09f
yudhies2109 <ma.yudhistira@hotmail.com>
2026-01-29 11:35:04 +0700

    devdependencies updated

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
